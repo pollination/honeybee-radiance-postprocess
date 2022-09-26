e396d2209b14338608f1b5efdf73c95a64f28ec6
Mikkel Pedersen <mkklpdrsn@gmail.com>
2022-09-26 09:26:30 +0200

    fix(leed): Change input name

--- pollination/honeybee_radiance_postprocess/leed.py
+++ pollination/honeybee_radiance_postprocess/leed.py
@@ -4,7 +4,14 @@ from pollination_dsl.function import Function, command, Inputs, Outputs
 
 @dataclass
 class DaylightOption1(Function):
-    """Calculate credits for LEED v4.1 Daylight Option 1."""
+    """Calculate credits for LEED v4.1 Daylight Option 1.
+
+    Use the shade-transmittance option to set a shade transmittance values for
+    aperture groups. The shade-transmittance-file option takes precedence over
+    the shade-transmittance, however, if any aperture groups are missing in the
+    JSON file given to the shade-transmittance-file option, the value from
+    shade-transmittance will be used for those aperture groups.
+    """
 
     folder = Inputs.folder(
         description='This folder is an output folder of annual daylight recipe. Folder '
@@ -14,25 +21,30 @@ class DaylightOption1(Function):
     )
 
     shade_transmittance = Inputs.float(
-        description='', default=0.2
+        description='A value to use as a multiplier in place of solar shading. Value '
+        'for shade transmittance must be 1 > value > 0.', default=0.2
     )
 
-    shade_transmittances = Inputs.file(
-        description='Path to an annual schedule file. Values should be 0-1 separated '
-        'by new line. If not provided an 8-5 annual schedule will be created.',
-        path='shade_transmittance.json', optional=True
+    shd_transmittance_file = Inputs.file(
+        description='A JSON file with a dictionary where aperture groups are keys, and '
+        'the value for each key is the shade transmittance. Values for shade '
+        'transmittance must be 1 > value > 0.',
+        path='shade_transmittance.json', extensions=['json'], optional=True
     )
 
     model = Inputs.file(
-        description='Path to input HBJSON file.',
+        description='Path to HBJSON file. The purpose of the model in this function is '
+        'to use the mesh area of the sensor grids to calculate area-weighted metrics. '
+        'In case no model is provided or the sensor grids in the model do not have any '
+        'mesh area, it will be assumed that all sensor points cover the same area.',
         path='model.hbjson', optional=True
     )
 
     @command
     def leed_daylight_option_1(self):
-        return 'honeybee-radiance-postprocess post-process leed daylight_option_1 ' \
+        return 'honeybee-radiance-postprocess post-process leed daylight-option-1 ' \
             'results --shade-transmittance {{self.shade_transmittance}} ' \
-            '--shade-transmittance-file {{self.shade_transmittances}} ' \
+            '--shade-transmittance-file "{{self.shd_transmittance_file}}" ' \
             '--sub-folder leed_summary'
 
     # outputs
